cdd692024d0451894c71a366aa71174dae1df105
leonardodoria23-cmyk <leonardodoria23@gmail.com>
2026-05-02 17:18:34 +0000

    🚀 Vivelo V5

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+export default { content: ["./index.html","./src/**/*.{js,jsx}"], theme: { extend: { colors: { caribbean: '#0A4A52', orange: '#FF8C42', yellow: '#FFD60A', cream: '#FFF8F0' } } }, plugins: [] }
